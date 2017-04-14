c34ca41599bf459504ed245bf48ed9bcd5e2ff53
mpasha96 <mukarram.pasha96@gmail.com>
2017-04-14 18:15:54 +0500

    Add files via upload

---
+++ 8 Number Puzzle Game/number game/Form1.Designer.cs
@@ -0,0 +1,255 @@
+﻿namespace number_game
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            this.button9 = new System.Windows.Forms.Button();
+            this.button10 = new System.Windows.Forms.Button();
+            this.button11 = new System.Windows.Forms.Button();
+            this.button12 = new System.Windows.Forms.Button();
+            this.button13 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.Silver;
+            this.button1.Location = new System.Drawing.Point(65, 78);
+            this.button1.Margin = new System.Windows.Forms.Padding(6);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(133, 139);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "7";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.Silver;
+            this.button2.Location = new System.Drawing.Point(197, 78);
+            this.button2.Margin = new System.Windows.Forms.Padding(6);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(133, 139);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "6";
+            this.button2.UseVisualStyleBackColor = false;
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.Color.Silver;
+            this.button3.Location = new System.Drawing.Point(326, 78);
+            this.button3.Margin = new System.Windows.Forms.Padding(6);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(133, 139);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "2";
+            this.button3.UseVisualStyleBackColor = false;
+            // 
+            // button4
+            // 
+            this.button4.BackColor = System.Drawing.Color.Silver;
+            this.button4.Location = new System.Drawing.Point(65, 211);
+            this.button4.Margin = new System.Windows.Forms.Padding(6);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(133, 139);
+            this.button4.TabIndex = 3;
+            this.button4.Text = "3";
+            this.button4.UseVisualStyleBackColor = false;
+            // 
+            // button5
+            // 
+            this.button5.BackColor = System.Drawing.Color.Silver;
+            this.button5.Location = new System.Drawing.Point(197, 211);
+            this.button5.Margin = new System.Windows.Forms.Padding(6);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(133, 139);
+            this.button5.TabIndex = 4;
+            this.button5.Text = "5";
+            this.button5.UseVisualStyleBackColor = false;
+            // 
+            // button6
+            // 
+            this.button6.BackColor = System.Drawing.Color.Silver;
+            this.button6.Location = new System.Drawing.Point(326, 211);
+            this.button6.Margin = new System.Windows.Forms.Padding(6);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(133, 139);
+            this.button6.TabIndex = 5;
+            this.button6.Text = "4";
+            this.button6.UseVisualStyleBackColor = false;
+            // 
+            // button7
+            // 
+            this.button7.BackColor = System.Drawing.Color.Silver;
+            this.button7.Location = new System.Drawing.Point(65, 347);
+            this.button7.Margin = new System.Windows.Forms.Padding(6);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(133, 139);
+            this.button7.TabIndex = 6;
+            this.button7.Text = "1";
+            this.button7.UseVisualStyleBackColor = false;
+            // 
+            // button8
+            // 
+            this.button8.BackColor = System.Drawing.Color.Silver;
+            this.button8.Location = new System.Drawing.Point(197, 347);
+            this.button8.Margin = new System.Windows.Forms.Padding(6);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(133, 139);
+            this.button8.TabIndex = 7;
+            this.button8.UseVisualStyleBackColor = false;
+            // 
+            // button9
+            // 
+            this.button9.BackColor = System.Drawing.Color.Silver;
+            this.button9.Location = new System.Drawing.Point(326, 347);
+            this.button9.Margin = new System.Windows.Forms.Padding(6);
+            this.button9.Name = "button9";
+            this.button9.Size = new System.Drawing.Size(133, 139);
+            this.button9.TabIndex = 8;
+            this.button9.Text = "8";
+            this.button9.UseVisualStyleBackColor = false;
+            // 
+            // button10
+            // 
+            this.button10.BackColor = System.Drawing.Color.Tan;
+            this.button10.Location = new System.Drawing.Point(664, 327);
+            this.button10.Margin = new System.Windows.Forms.Padding(6);
+            this.button10.Name = "button10";
+            this.button10.Size = new System.Drawing.Size(133, 89);
+            this.button10.TabIndex = 9;
+            this.button10.Text = "DOWN";
+            this.button10.UseVisualStyleBackColor = false;
+            this.button10.Click += new System.EventHandler(this.button10_Click);
+            // 
+            // button11
+            // 
+            this.button11.BackColor = System.Drawing.Color.Tan;
+            this.button11.Location = new System.Drawing.Point(664, 151);
+            this.button11.Margin = new System.Windows.Forms.Padding(6);
+            this.button11.Name = "button11";
+            this.button11.Size = new System.Drawing.Size(133, 89);
+            this.button11.TabIndex = 10;
+            this.button11.Text = "UP";
+            this.button11.UseVisualStyleBackColor = false;
+            this.button11.Click += new System.EventHandler(this.button11_Click);
+            // 
+            // button12
+            // 
+            this.button12.BackColor = System.Drawing.Color.Tan;
+            this.button12.Location = new System.Drawing.Point(532, 237);
+            this.button12.Margin = new System.Windows.Forms.Padding(6);
+            this.button12.Name = "button12";
+            this.button12.Size = new System.Drawing.Size(133, 89);
+            this.button12.TabIndex = 11;
+            this.button12.Text = "LEFT";
+            this.button12.UseVisualStyleBackColor = false;
+            this.button12.Click += new System.EventHandler(this.button12_Click);
+            // 
+            // button13
+            // 
+            this.button13.BackColor = System.Drawing.Color.Tan;
+            this.button13.Location = new System.Drawing.Point(794, 237);
+            this.button13.Margin = new System.Windows.Forms.Padding(6);
+            this.button13.Name = "button13";
+            this.button13.Size = new System.Drawing.Size(133, 89);
+            this.button13.TabIndex = 12;
+            this.button13.Text = "RIGHT";
+            this.button13.UseVisualStyleBackColor = false;
+            this.button13.Click += new System.EventHandler(this.button13_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.BackColor = System.Drawing.Color.White;
+            this.textBox1.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.Location = new System.Drawing.Point(603, 461);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(262, 25);
+            this.textBox1.TabIndex = 13;
+            this.textBox1.Text = "Made By: Mukarram, Sohaib & Salman";
+            this.textBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(13F, 30F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Black;
+            this.ClientSize = new System.Drawing.Size(959, 540);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button13);
+            this.Controls.Add(this.button12);
+            this.Controls.Add(this.button11);
+            this.Controls.Add(this.button10);
+            this.Controls.Add(this.button9);
+            this.Controls.Add(this.button8);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Font = new System.Drawing.Font("Segoe UI", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(6);
+            this.Name = "Form1";
+            this.Text = "Number Puzzle Game";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.Button button9;
+        private System.Windows.Forms.Button button10;
+        private System.Windows.Forms.Button button11;
+        private System.Windows.Forms.Button button12;
+        private System.Windows.Forms.Button button13;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
+
---
+++ 8 Number Puzzle Game/number game/Form1.cs
@@ -0,0 +1,218 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace number_game
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+        int count = 0;
+
+        //up button
+        private void button10_Click(object sender, EventArgs e)
+        {
+            count = 1;
+
+            if (button4.Text == "" && count == 1)
+            {
+                button4.Text = button1.Text;
+                button1.Text = "";
+                count = count + 1;
+
+
+            }
+            if (button5.Text == "" && count == 1)
+            {
+                button5.Text = button2.Text;
+                button2.Text = "";
+                count = count + 1;
+            }
+            if (button6.Text == "" && count == 1)
+            {
+                button6.Text = button3.Text;
+                button3.Text = "";
+                count = count + 1;
+            }
+            if (button7.Text == "" && count == 1)
+            {
+                button7.Text = button4.Text;
+                button4.Text = "";
+                count = count + 1;
+            }
+            if (button8.Text == "" && count == 1)
+            {
+                button8.Text = button5.Text;
+                button5.Text = "";
+                count = count + 1;
+            }
+            if (button9.Text == "" && count == 1)
+            {
+                button9.Text = button6.Text;
+                button6.Text = "";
+                count = count + 1;
+            }
+        }
+
+        //down button
+        private void button11_Click(object sender, EventArgs e)
+        {
+            count = 1;
+            if (button1.Text == "" && count == 1)
+            {
+                button1.Text = button4.Text;
+                button4.Text = "";
+                count = count + 1;
+            }
+            if (button2.Text == "" && count == 1)
+            {
+                button2.Text = button5.Text;
+                button5.Text = "";
+                count = count + 1;
+            }
+            if (button3.Text == "" && count == 1)
+            {
+                button3.Text = button6.Text;
+                button6.Text = "";
+                count = count + 1;
+            }
+            if (button4.Text == "" && count == 1)
+            {
+                button4.Text = button7.Text;
+                button7.Text = "";
+                count = count + 1;
+            }
+            if (button5.Text == "" && count == 1)
+            {
+                button5.Text = button8.Text;
+                button8.Text = "";
+                count = count + 1;
+            }
+            if (button6.Text == "" && count == 1)
+            {
+                button6.Text = button9.Text;
+                button9.Text = "";
+                count = count + 1;
+            }
+            win();
+        }
+
+        //right button
+        private void button12_Click(object sender, EventArgs e)
+        {
+            count = 1;
+            if (button1.Text == "" && count == 1)
+            {
+                button1.Text = button2.Text;
+                button2.Text = "";
+                count = count + 1;
+            }
+            if (button2.Text == "" && count == 1)
+            {
+                button2.Text = button3.Text;
+                button3.Text = "";
+                count = count + 1;
+            }
+            if (button4.Text == "" && count == 1)
+            {
+                button4.Text = button5.Text;
+                button5.Text = "";
+                count = count + 1;
+            }
+            if (button5.Text == "" && count == 1)
+            {
+                button5.Text = button6.Text;
+                button6.Text = "";
+                count = count + 1;
+            }
+            if (button7.Text == "" && count == 1)
+            {
+                button7.Text = button8.Text;
+                button8.Text = "";
+                count = count + 1;
+            }
+            if (button8.Text == "" && count == 1)
+            {
+                button8.Text = button9.Text;
+                button9.Text = "";
+                count = count + 1;
+            }
+            win();
+        }
+
+        //left button
+        private void button13_Click(object sender, EventArgs e)
+        {
+            count = 1;
+            if (button2.Text == "" && count == 1)
+            {
+                button2.Text = button1.Text;
+                button1.Text = "";
+                count = count + 1;
+            }
+            if (button5.Text == "" && count == 1)
+            {
+                button5.Text = button4.Text;
+                button4.Text = "";
+                count = count + 1;
+            }
+            if (button8.Text == "" && count == 1)
+            {
+                button8.Text = button7.Text;
+                button7.Text = "";
+                count = count + 1;
+            }
+            if (button3.Text == "" && count == 1)
+            {
+                button3.Text = button2.Text;
+                button2.Text = "";
+                count = count + 1;
+            }
+            if (button6.Text == "" && count == 1)
+            {
+                button6.Text = button5.Text;
+                button5.Text = "";
+                count = count + 1;
+            }
+            if (button9.Text == "" && count == 1)
+            {
+                button9.Text = button8.Text;
+                button8.Text = "";
+                count = count + 1;
+            }
+            win();
+        }
+
+        //conditions for win
+        public void win()
+        {
+            if (button1.Text == "1" && button2.Text == "2" && button3.Text == "3" && button4.Text == "4" && button5.Text == "5" && button6.Text == "6" && button7.Text == "7" && button8.Text == "8" && button9.Text == "")
+            {
+                //disabling all buttons after win
+                button10.Enabled = false;
+                button11.Enabled = false;
+                button12.Enabled = false;
+                button13.Enabled = false;
+                MessageBox.Show("Congratulations!!! You Won The 8 Number Puzzle");
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
